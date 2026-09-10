057cc82975f156cf4e72a331935791413df4d593
jianrossdr <jiandelarosa21@yahoo.com>
2026-09-10 18:39:07 +0800

    fix: register view services in HTTP entrypoint

--- public/index.php
+++ public/index.php
@@ -17,4 +17,7 @@ require __DIR__.'/../vendor/autoload.php';
 /** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
+$app->register(\Illuminate\View\ViewServiceProvider::class);
+$app->register(\Inertia\ServiceProvider::class);
+
 $app->handleRequest(Request::capture());
